9f54e09a6aa73b9dfbeeb79b6b004765b8ce79a0
stefankoch <stefankoch@141.244.28.127>
2016-03-02 17:01:48 +0100

    Basic Table Operations Input Tsv Parser TreeTableView Adduct View Next Button RT in Minutes Fixed EIC Open multiple mzXML

--- src/main/java/com/mycompany/fxmltableview/ChartGenerator.java
+++ src/main/java/com/mycompany/fxmltableview/ChartGenerator.java
@@ -7,20 +7,23 @@ package com.mycompany.fxmltableview;
 
 import static java.lang.Math.abs;
 import java.util.Collections;
+import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Tooltip;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 /**
  *
- * @author stefankoch
- * TODO:
- * Just read every slice once and generate all the different graphs
- * 
+ * @author stefankoch TODO: Just read every slice once and generate all the
+ * different graphs
+ *
  */
 public class ChartGenerator {
 
@@ -35,7 +38,6 @@ public class ChartGenerator {
         xAxis.setLabel("RT [minutes]");
         yAxis.setLabel("Intensity");
         LineChart<Number, Number> linechart = new LineChart(xAxis, yAxis);
-       
 
         // for all slices (= for all files)
         for (int i = 0; i < adduct.getListofSlices().size(); i++) {
@@ -43,110 +45,100 @@ public class ChartGenerator {
 
             XYChart.Series newSeries = new XYChart.Series();
 
-            
             //while the next RT is the same as the one before, add Intensities
             for (int j = 0; j < currentSlice.getIntensityList().size(); j++) {
                 float intensity = currentSlice.getIntensityList().get(j);
                 float currentRT = currentSlice.getRetentionTimeList().get(j);
-             while (j<currentSlice.getIntensityList().size()-1 && abs(currentRT-currentSlice.getRetentionTimeList().get(j+1))<0.0001 ){
-                 j++;
-                 intensity= intensity + currentSlice.getIntensityList().get(j);
-                 
-                 
-             } 
-                 
-                 newSeries.getData().add(new XYChart.Data(currentRT, intensity));
-             
+                while (j < currentSlice.getIntensityList().size() - 1 && abs(currentRT - currentSlice.getRetentionTimeList().get(j + 1)) < 0.0001) {
+                    j++;
+                    intensity = intensity + currentSlice.getIntensityList().get(j);
+
+                }
                 
-                
-                
+                XYChart.Data data = new XYChart.Data(currentRT, intensity);
+                Path path = new Path();
+                data.setNode(path);
+                path.setStroke(Color.BLACK);
+                System.out.println("Original" + path);
+                newSeries.getData().add(data);
+                    
                 
 
             }
+            
+
             linechart.getData().add(newSeries);
             linechart.setCreateSymbols(false);
             linechart.setMaxSize(300, 200);
-            
+
             //set Range
-   
-             xAxis.setAutoRanging(false);
-            xAxis.setTickUnit((adduct.getListofSlices().get(0).getMaxRT()-adduct.getListofSlices().get(0).getMinRT())/7);
+            xAxis.setAutoRanging(false);
+            xAxis.setTickUnit((adduct.getListofSlices().get(0).getMaxRT() - adduct.getListofSlices().get(0).getMinRT()) / 7);
             xAxis.setLowerBound(adduct.getListofSlices().get(0).getMinRT());
             xAxis.setUpperBound(adduct.getListofSlices().get(0).getMaxRT());
 
         }
-linechart.setLegendVisible(false);
+        linechart.setLegendVisible(false);
         return linechart;
     }
 
-    
     public LineChart generateNormalizedEIC(Entry adduct) {
-        
+
         //Basic Chart attributes
         NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("RT [minutes]");
         yAxis.setLabel("Intensity (normalized)");
         LineChart<Number, Number> linechart = new LineChart(xAxis, yAxis);
-        
-        
+
         // for all slices (= for all files)
         for (int i = 0; i < adduct.getListofSlices().size(); i++) {
             Slice currentSlice = adduct.getListofSlices().get(i);
 
             XYChart.Series newSeries = new XYChart.Series();
 
-            
             float maxIntensity = Collections.max(currentSlice.getIntensityList());
-            
+
             for (int j = 0; j < currentSlice.getIntensityList().size(); j++) {
                 float intensity = currentSlice.getIntensityList().get(j);
                 float currentRT = currentSlice.getRetentionTimeList().get(j);
-             while (j<currentSlice.getIntensityList().size()-1 && abs(currentRT-currentSlice.getRetentionTimeList().get(j+1))<0.0001 ){
-                 j++;
-                 intensity= intensity + currentSlice.getIntensityList().get(j);
-                 
-                 
-             } 
-                 
-                 newSeries.getData().add(new XYChart.Data(currentRT, intensity/maxIntensity));
-             
-                
-                
-                
-                
+                while (j < currentSlice.getIntensityList().size() - 1 && abs(currentRT - currentSlice.getRetentionTimeList().get(j + 1)) < 0.0001) {
+                    j++;
+                    intensity = intensity + currentSlice.getIntensityList().get(j);
+
+                }
+
+                newSeries.getData().add(new XYChart.Data(currentRT, intensity / maxIntensity));
 
             }
             linechart.getData().add(newSeries);
             linechart.setCreateSymbols(false);
             linechart.setMaxSize(300, 200);
-            
+
             //set Range
-             xAxis.setAutoRanging(false);
-            xAxis.setTickUnit((adduct.getListofSlices().get(0).getMaxRT()-adduct.getListofSlices().get(0).getMinRT())/7);
+            xAxis.setAutoRanging(false);
+            xAxis.setTickUnit((adduct.getListofSlices().get(0).getMaxRT() - adduct.getListofSlices().get(0).getMinRT()) / 7);
             xAxis.setLowerBound(adduct.getListofSlices().get(0).getMinRT());
             xAxis.setUpperBound(adduct.getListofSlices().get(0).getMaxRT());
-            
+
             yAxis.setAutoRanging(false);
             yAxis.setLowerBound(0);
             yAxis.setUpperBound(1);
-
 
         }
 
         linechart.setLegendVisible(false);
         return linechart;
     }
-    
+
     public ScatterChart generateMassChart(Entry adduct) {
-        
+
         //Basic Chart attributes
         NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("RT [minutes]");
         yAxis.setLabel("Mass");
         ScatterChart<Number, Number> scatterchart = new ScatterChart(xAxis, yAxis);
-        
 
         // for all slices (= for all files)
         for (int i = 0; i < adduct.getListofSlices().size(); i++) {
@@ -159,39 +151,40 @@ linechart.setLegendVisible(false);
                 if (currentSlice.getMassList().get(j) != null) {
                     XYChart.Data data = new XYChart.Data(currentSlice.getRetentionTimeList().get(j), currentSlice.getMassList().get(j));
                     Rectangle rect1 = new Rectangle(5, 5);
-                    rect1.setFill(Color.RED);
-                    data.setNode(rect1);
-                    data.getNode().setOpacity(currentSlice.getIntensityList().get(j)/maxIntensity);
-                    Tooltip tooltip = new Tooltip();
-            tooltip.setText(currentSlice.getIntensityList().get(j).toString());
-            Tooltip.install(data.getNode(), tooltip);
                     
+                    if (i==0) {
+                        rect1.setFill(Color.RED);
+                        
+                    }else {
+                    rect1.setFill(Color.GREEN);}
+                    data.setNode(rect1);
+                    data.getNode().setOpacity(currentSlice.getIntensityList().get(j) / maxIntensity);
+                    Tooltip tooltip = new Tooltip();
+                    tooltip.setText(currentSlice.getIntensityList().get(j).toString());
+                    Tooltip.install(data.getNode(), tooltip);
+
                     newSeries.getData().add(data);
                 }
-                
 
             }
             scatterchart.getData().add(newSeries);
-            
+
             scatterchart.setMaxSize(300, 200);
-            
+
             //set Range
-             xAxis.setAutoRanging(false);
-            xAxis.setTickUnit((adduct.getListofSlices().get(0).getMaxRT()-adduct.getListofSlices().get(0).getMinRT())/7);
+            xAxis.setAutoRanging(false);
+            xAxis.setTickUnit((adduct.getListofSlices().get(0).getMaxRT() - adduct.getListofSlices().get(0).getMinRT()) / 7);
             xAxis.setLowerBound(adduct.getListofSlices().get(0).getMinRT());
             xAxis.setUpperBound(adduct.getListofSlices().get(0).getMaxRT());
-            
+
             yAxis.setAutoRanging(false);
-            yAxis.setTickUnit((adduct.getListofSlices().get(0).getMaxMZ()-adduct.getListofSlices().get(0).getMinMZ())/5);
+            yAxis.setTickUnit((adduct.getListofSlices().get(0).getMaxMZ() - adduct.getListofSlices().get(0).getMinMZ()) / 5);
             yAxis.setLowerBound(adduct.getListofSlices().get(0).getMinMZ());
             yAxis.setUpperBound(adduct.getListofSlices().get(0).getMaxMZ());
-            
-            
-           
 
         }
-scatterchart.setLegendVisible(false);
+        scatterchart.setLegendVisible(false);
         return scatterchart;
- 
+
     }
 }
--- src/main/java/com/mycompany/fxmltableview/Fxml_adductviewController.java
+++ src/main/java/com/mycompany/fxmltableview/Fxml_adductviewController.java
@@ -7,6 +7,7 @@ package com.mycompany.fxmltableview;
  */
 
 import com.mycompany.fxmltableview.Entry;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -21,6 +22,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TreeTableView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Rectangle;
 
 /**
  * FXML Controller class
@@ -36,18 +40,19 @@ public class Fxml_adductviewController implements Initializable {
     
     
     TreeTableView<Entry> metTable;
+    ChartGenerator chartGenerator;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-      
+      chartGenerator = new ChartGenerator();
     }    
     
     
     public void print() {
-        ChartGenerator chartGenerator = new ChartGenerator();
+        
        Entry entry = metTable.getSelectionModel().getSelectedItem().getValue();
        gridPane.getChildren().clear();
        for (int i = 0; i<entry.getListofAdducts().size(); i++) {
@@ -55,14 +60,22 @@ public class Fxml_adductviewController implements Initializable {
            Label label = new Label(Double.toString(entry.getListofAdducts().get(i).getMZ()));
            label.setRotate(270);
            gridPane.addRow(i,label);
-           LineChart<Number,Number> linechart = chartGenerator.generateEIC(entry.getListofAdducts().get(i));
-           gridPane.addColumn(1,linechart);
-           linechart = chartGenerator.generateNormalizedEIC(entry.getListofAdducts().get(i));
-           gridPane.addColumn(2, linechart);
+           LineChart<Number,Number> linechart1 = chartGenerator.generateEIC(entry.getListofAdducts().get(i));
+           gridPane.addColumn(1,linechart1);
+           LineChart<Number,Number> linechart2 = chartGenerator.generateNormalizedEIC(entry.getListofAdducts().get(i));
+           gridPane.addColumn(2, linechart2);
            ScatterChart<Number,Number> scatterchart = chartGenerator.generateMassChart(entry.getListofAdducts().get(i));
            gridPane.addColumn(3, scatterchart);
+           System.out.println("new" + linechart1.getData().get(0).getNode().getId());
+           System.out.println(scatterchart.getData().get(0).getData().get(0).getNode().getClass());
+           System.out.println(linechart1.getData().get(0).getNode().getClass());
+           System.out.println(((Path)linechart1.getData().get(0).getNode()).getStroke());
+           System.out.println(((Rectangle)scatterchart.getData().get(0).getData().get(0).getNode()).getFill());
+           System.out.println(((Path)linechart1.getData().get(0).getNode()).getStrokeWidth());
+        
        }
        
+      
      
         
     }
